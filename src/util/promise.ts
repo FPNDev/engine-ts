734fbfe7ebb2e1bdac220181025449abb1b2146e
fpn <fpndev@gmail.com>
2025-09-24 11:49:37 +0300

    update retry util

--- src/util/promise.ts
+++ src/util/promise.ts
@@ -4,11 +4,11 @@ const retry = <T>(fn: () => Promise<T>, tries = 3, timeout = 0): Promise<T> => {
         timeout
           ? new Promise((resolve) =>
               window.setTimeout(
-                () => resolve(retry(fn, tries, timeout)),
-                timeout,
-              ),
+                () => retry(fn, tries, timeout).then(resolve),
+                timeout
+              )
             )
-          : retry(fn, tries, timeout),
+          : retry(fn, tries, timeout)
       )
     : fn();
 };
