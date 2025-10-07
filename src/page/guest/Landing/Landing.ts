0de2a0112aa45e26685ff180da1186d08172c6e6
fpn <fpndev@gmail.com>
2025-10-07 17:27:18 +0300

    update engine, add props passthrough to view

--- src/page/guest/Landing/Landing.ts
+++ src/page/guest/Landing/Landing.ts
@@ -4,7 +4,7 @@ import { html } from "../../../local_modules/util/dom-manipulation";
 import classes from './style.module.scss';
 
 export class Landing extends Component {
-  render(): Node {
+  view(): Node {
     return html`
       <div class="${classes.landingTitle}">Welcome</div>
     `;
